e8039f0dd5d8d734162f5c054048e413ae6ac00d
Michele Bini <michele.bini@gmail.com>
2014-10-25 13:38:28 +0200

    Test cases and interactive testing script

---
+++ interactive-test.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+println() {
+    printf "%s\n" "$*"
+}
+
+warn() {
+    println "$0: $*" >&2
+}
+
+die() {
+    warn "$*"
+    exit 1
+}
+
+
+git diff --quiet -- test-cases/ || die "There are unstaged changes in test-cases/; please stage or discard them before running $0 again!"
+coffeemake --rebuild test-cases/*
+git add -up test-cases/
+
